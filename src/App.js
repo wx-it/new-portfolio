42d8d4dcf2febe0d116484894889f999a7c41335
Lix <zinebbendjafer9@gmail.com>
2023-01-05 21:19:05 +0100

    added a loader spinner + favicon and changed title

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Loader from "./components/Loader";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
   return (
-    <div className="App">
-     <Header />
-     <Projects />
-     <Contact />
-    </div>
+    <>
+     {
+      loading ? (
+        <Loader/>
+      ) : (
+        <div>
+          <Header />
+          <Projects />
+          <Contact />
+        </div>
+      )
+     }
+    </>
   );
 }
 
---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className="content">
+       <div className="loading">
+            <p>Loading</p>
+            <span></span>
+        </div>
+    </div>
+  )
+}
+
+export default Loader
