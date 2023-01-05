83ec0f16b37b98f565574fce8dcaa3c846116b98
Lix <zinebbendjafer9@gmail.com>
2023-01-05 21:26:59 +0100

    adding data

---
+++ src/assets/data/projects.js
@@ -0,0 +1,9 @@
+export default [
+    {
+        id: 1,
+        title: "Quizzical",
+        description: "a quiz app made with React and quiz API.",
+        languages: "React",
+        link: ""
+    }
+]
