e23ab86b8bc2587ae8fac217a5d9c862689fb8e2
Lix <zinebbendjafer9@gmail.com>
2023-01-05 20:02:44 +0100

    mobile projects section done styling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import GrayLink from "../assets/gray-link.svg"
 
 const Projects = () => {
   return (
@@ -7,7 +7,40 @@ const Projects = () => {
 
         <div className='projects--cards'>
             <div className='projects--card'>
-                <p>React</p>
+                <div className="link">
+                  <p>React</p>
+                  <a href="">
+                      <img src={GrayLink} alt="" />
+                  </a>
+                </div>
+                <h2>Quizzical</h2>
+                <p>
+                  a quiz app made with React and quiz API.
+                </p>
+            </div>
+
+
+            <div className='projects--card'>
+                <div className="link">
+                  <p>React</p>
+                  <a href="">
+                      <img src={GrayLink} alt="" />
+                  </a>
+                </div>
+                <h2>Quizzical</h2>
+                <p>
+                  a quiz app made with React and quiz API.
+                </p>
+            </div>
+
+
+            <div className='projects--card'>
+                <div className="link">
+                  <p>React</p>
+                  <a href="">
+                      <img src={GrayLink} alt="" />
+                  </a>
+                </div>
                 <h2>Quizzical</h2>
                 <p>
                   a quiz app made with React and quiz API.
