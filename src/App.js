44f584dabfd16ea6414ea0401e1f232e350be41d
Lix <zinebbendjafer9@gmail.com>
2023-01-05 20:41:51 +0100

    mobile contact form styling done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
      <Header />
      <Projects />
+     <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+        <h2>CONTACT</h2>
+        <form action="">
+            <input type="text" placeholder='Name' />
+            <input type="text" placeholder='Email' />
+            <input type="text" placeholder='Subject' />
+            <input type="text" placeholder='Message' />
+        </form>
+    </div>
+  )
+}
+
+export default Contact
