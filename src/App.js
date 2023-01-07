c97e5d4f97be981e88fcf58f347d01e0bcb79715
Lix <zinebbendjafer9@gmail.com>
2023-01-07 12:36:46 +0100

    styling desktop nav

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Loader from "./components/Loader";
+import DesktopNav from "./components/DesktopNav";
 import { useState, useEffect } from "react";
 
 function App() {
@@ -20,10 +21,15 @@ function App() {
       loading ? (
         <Loader/>
       ) : (
-        <div>
+        <div className="container">
+          <div className="left">
           <Header />
+          <DesktopNav />
+          </div>
+          <div className="tabs">
           <Projects />
           <Contact />
+          </div>
         </div>
       )
      }
---
+++ src/components/DesktopNav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const DesktopNav = () => {
+  return (
+    <nav>
+        <div className='tab'>
+            <span>1</span>
+            <span className='line'></span>
+            <a href="">PROJECTS</a>
+        </div>
+        
+        <div className='tab'>
+            <span>2</span>
+            <span className='line'></span>
+            <a href="">CONTACT</a>
+        </div>
+    </nav>
+  )
+}
+
+export default DesktopNav
