85e47025c3ed007ac5bdd6e759709e3e14f0ddd6
Lix <zinebbendjafer9@gmail.com>
2023-04-23 17:47:09 +0100

    changing everything

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <header>
+      <div>Logo</div>
+      <nav>
+        <div>
+          <a href="">About</a>
+          <a href="">Services</a>
+          <a href="">Projects</a>
+          <a href="">Contact</a>
+        </div>
+        <button>Resume</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
