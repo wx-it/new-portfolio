99b90505e497452716626d2f482d9562234c62bf
Lix <zinebbendjafer9@gmail.com>
2023-04-23 18:22:50 +0100

    added mobile and desktop sidebar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import "./navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const toggleNav = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <header>
       <div>Logo</div>
-      <nav>
+      <button onClick={toggleNav} className="toggleNavBtn">toggle</button>
+      <nav className={toggle ? "toggle-nav" : "hide-nav"}>
         <div>
           <a href="">About</a>
           <a href="">Services</a>
