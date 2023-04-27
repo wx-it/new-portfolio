24cdcd79536a603547426755848f575ce0480fbf
Lix <zinebbendjafer9@gmail.com>
2023-04-27 17:46:07 +0100

    done with header and navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,19 +5,22 @@ const Header = () => {
   return (
     <div className="header">
       <div>
-        <h1>My name is Zineb,</h1>
-        <h1>I help startups build their website</h1>
+        <h1>My name is Zineb, I help startups build their website. </h1>
+        <p>
+          I'm a Junior Frontend web Developer with so much passion. always ready
+          for a challenge and new projects, here to help and here to work. I'm
+          currently working my way towards becoming a MERN stack developer.
+        </p>
+        <p>
+          I'm a Junior Frontend web Developer with so much passion for learning
+          tech and all the newest technologies
+        </p>
         <p>
           I'm a Junior Frontend web Developer with so much passion for learning
           tech and all the newest technologies, always ready for a challenge and
-          new projects, here to help and here to work. I'm currently working my
-          way towards becoming a MERN stack developer.
+          new projects, here to help and here to work.
         </p>
       </div>
-        <div>
-          <button>My Story</button>
-          <button>See Work</button>
-        </div>
     </div>
   );
 };
