666fa857f8c2661b585768222e8f4a7c7ae1c6d8
Lix <zinebbendjafer9@gmail.com>
2023-04-27 17:21:13 +0100

    changing navbar styling

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -24,17 +24,19 @@ const Services = () => {
           </p>
         </div>
         <div className="service">
-          <h3>Responsive web design</h3>
+          <h3>User interface design</h3>
           <p>
-            Creating websites that can adapt to different screen sizes and
-            devices, ensuring a seamless user experience on all layouts.
+            Designing the interface that users interact with when using a
+            website, including the layout, typography, colors, and overall
+            visual style.
           </p>
         </div>
         <div className="service">
-          <h3>Responsive web design</h3>
+          <h3>Front-end development</h3>
           <p>
-            Creating websites that can adapt to different screen sizes and
-            devices, ensuring a seamless user experience on all layouts.
+            Writing the HTML, CSS, and JavaScript / Reactjs code that makes up
+            the front end of a website, ensuring that it's functional, fast, and
+            user-friendly.
           </p>
         </div>
       </div>
